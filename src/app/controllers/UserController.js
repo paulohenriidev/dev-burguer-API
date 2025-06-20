47d873f7f32e76327d358ffe1e6423e5c3940b85
paulohenriidev <paulohenriidev@gmail.com>
2025-06-20 19:45:29 -0300

    Validate process

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,24 +1,38 @@
 import User from "../models/User";
 import { v4 } from "uuid";
+import * as Yup from "yup";
 
 class UserController {
   async store(req, res) {
-    const { name, email, password_hash, admin } = req.body;
-      const user = await User.create({
-        id: v4(),
-        name,
-        email,
-        password_hash,
-        admin,
-      });
+    const schema = Yup.object({
+      name: Yup.string().required().min(3),
+      email: Yup.string().required().email(),
+      password_hash: Yup.string().min(8).required(),
+      admin: Yup.boolean(),
+    });
 
-      return res.status(201).json({
-        id: user.id,
-        name,
-        email,
-      });
-      
+    try {
+      schema.validateSync(req.body, {abortEarly: false})
+    } catch (error) {
+      return res.status(400).json({error: error.errors})
     }
-  }
+    
+    const { name, email, password_hash, admin } = req.body;
+    const user = await User.create({
+      id: v4(),
+      name,
+      email,
+      password_hash,
+      admin,
+    });
 
-export default new UserController()
+    return res.status(201).json({
+      id: user.id,
+      name,
+      email,
+      admin,
+    });
+  }
+}
+
+export default new UserController();
